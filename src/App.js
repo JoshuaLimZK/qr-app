a40cd8f6926e697e72b759f4469f02b55ae4c7d6
Joshua Lim Zhe Kai <joshua_lim_zhe_kai@s2019.ssts.edu.sg>
2021-07-29 23:18:10 +0800

    Added QR generating Capability
    Poggers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import style from './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
 import { TextField } from '@material-ui/core';
 import { Button } from '@material-ui/core'
 
 function App() {
+
+  const [field, setField] = useState('');
+  const [fieldError, setFieldError] = useState(false);
+  const [call, setcall] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setFieldError(false)
+
+    if (field === "")  {
+      setFieldError(true)
+    }  else {
+      console.log(field)
+      setcall('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + field)
+      console.log(call)
+    }
+  }
+
   return (
-    <div className="App" id={style.main}>
+    <div className="App" id={styles.div}>
       <TextField
-          id="url"
-          label="URL"
-          style={{ margin: 8 }}
-          placeholder="Write the URL to be converted"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <Button>GENERATE!</Button>
+        onChange = {(e) => setField(e.target.value)}
+        id="url"
+        label="URL"
+        style={{ margin: 8,}}
+        margin="normal"
+        variant="outlined"
+        error={fieldError}
+        fullWidth
+      />
+      <Button
+      onClick = {handleSubmit}
+      >
+        GENERATE!
+      </Button>
+      <img src = {call} alt = "" width = {250} height = {250}/>
     </div>
   );
 }
